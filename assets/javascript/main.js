c270adf25f06518a31cae138db691fc8e7c0a75c
Sara Richardson <63122520+SaraRichardson129@users.noreply.github.com>
2020-05-15 18:06:40 -0500

    update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,16 +9,15 @@ let resetButton = document.querySelector('#reset')
 
 rollButton.addEventListener("click", function () {
     let diceInput = document.querySelector('#dice-number').value
+
     let counter = 0
     let total = 0
     while (counter < userInput.value) {
-
         diceInput = Math.floor(Math.random() * 6) + 1;
         counter += 1
         total += diceInput
         console.log(diceInput)
         dieRolls.push(diceInput);
-
     }
 
     totalValue.innerHTML = 'The sum of the rolls is ' + total
@@ -26,20 +25,16 @@ rollButton.addEventListener("click", function () {
 
 
 allRollsButton.addEventListener("click", function () {
+
     let counter = 0
-
     while (counter < dieRolls.length) {
-
         listElement.innerHTML += "<li>" + dieRolls[counter] + "</li>";
-
         counter += 1
     }
-
 })
 
 resetButton.addEventListener('click', function () {
     userInput.value = newCounterValue = ' ';
     totalValue.innerHTML = newCounterValue = 'The sum of the rolls is ' + 0;
     listElement.innerHTML = newCounterValue = ' ';
-
 })
